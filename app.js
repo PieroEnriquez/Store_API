90b57e9b79ed374b5824831707bcee1259bcf658
Piero Enriquez <pieroenriquezvera@gmail.com>
2022-09-10 16:39:51 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+//async errors
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+const port = process.env.PORT || 3000
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products routes</a>')
+})
+
+
+
+//products route
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const start = async() => {
+    try {
+        //connect db
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
