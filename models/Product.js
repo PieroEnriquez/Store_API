e67c1d303257b9c30b26ac4ba302f4bcaf7fa070
Piero Enriquez <pieroenriquezvera@gmail.com>
2022-09-10 19:13:48 -0500

    Creating schema for db

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const ProductSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Must provide name'],
-        trim: true,
-        maxlength: [20, 'Name cannot be more than 20 characters']
     },
-    completed: {
+    price: {
+        type: Number,
+        required: [true, 'Must provide price'],
+    },
+    featured: {
         type: Boolean,
         default: false
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    company: {
+        type: String,
+        enum: {
+            values: ['ikea', 'liddy', 'caressa', 'marcos'],
+            message: '{VALUE} is not supported'
+        }
     }
 });
 
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('Product', productSchema)
